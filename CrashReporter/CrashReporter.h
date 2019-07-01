81339e0eadc207281c07b839a018948541683fef
Christian Tietze <christian.tietze@gmail.com>
2019-07-01 10:42:03 +0200

    add Xcode project

---
+++ CrashReporter/CrashReporter.h
@@ -0,0 +1,13 @@
+//  Copyright © 2019 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CrashReporter.
+FOUNDATION_EXPORT double CrashReporterVersionNumber;
+
+//! Project version string for CrashReporter.
+FOUNDATION_EXPORT const unsigned char CrashReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrashReporter/PublicHeader.h>
+
+
