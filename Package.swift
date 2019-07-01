5442be278f059291c0235e63fd0977a47b6dc752
Christian Tietze <christian.tietze@gmail.com>
2019-07-01 21:06:39 +0200

    add SwiftPM manifest

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+
+//  Copyright © 2019 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import PackageDescription
+
+let package = Package(
+    name: "CrashReporter",
+    platforms: [
+        .macOS(.v10_12)
+    ],
+    products: [
+        .library(name: "CrashReporter", targets: ["CrashReporter"])
+    ],
+    targets: [
+        .target(name: "CrashReporter"),
+        .testTarget(
+            name: "CrashReporterTests",
+            dependencies: ["CrashReporter"]
+        )
+    ]
+)
